878c8798efcd6187e5e4ec13ecc796f5b1496364
Heitor <heitorbolisw4@gmail.com>
2026-06-20 20:22:49 -0300

    feat: configured database context

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -10,5 +10,15 @@ namespace Data
         }
 
         public DbSet<ShortenedUrl> ShortenedUrls { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ShortenedUrl>(builder =>
+            {
+                builder.Property(shortenedUrl => shortenedUrl.Code).HasMaxLength(ShortLinkSettings.Length);
+                builder.HasIndex(shortenedUrl => shortenedUrl.Code).IsUnique();
+            });
+
+        }
     }
 }
---
+++ Entities/ShortLinkSettings.cs
@@ -0,0 +1,8 @@
+namespace Entities
+{
+    public class ShortLinkSettings
+    {
+        public const int Length = 7;
+        public const string Alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    }
+}
